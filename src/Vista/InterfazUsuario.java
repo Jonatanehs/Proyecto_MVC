e3d12752a6931c22fdc853bee6c491198a4f2208
Leidy Johanna Bolivar Gonzalez <140461573+JohannaB95@users.noreply.github.com>
2024-06-20 18:46:06 -0500

    Update InterfazUsuario.java

--- src/Vista/InterfazUsuario.java
+++ src/Vista/InterfazUsuario.java
@@ -3,7 +3,7 @@ package Vista;
 import Modelo.GestionArray;
 import Controlador.Controlador;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 import java.util.Scanner;
 
 public class InterfazUsuario {
@@ -12,117 +12,90 @@ public class InterfazUsuario {
     Scanner x = new Scanner(System.in);
 
     public int menuPrincipal(){
-
-        System.out.println("\n1. Registrarse.");
-        System.out.println("2. Iniciar Sesión.");
-        System.out.println("3. Iniciar sesión como administrador.");
-        System.out.println("4. Salir.");
-        System.out.println("Elija una opción:");
-        opcion = x.nextInt();
-
+        try {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("\n1. Registrarse. \n2. Iniciar Sesión. \n3. Salir. " +
+                    "\nElija una opción:"));
+        }catch (NumberFormatException e) {
+            JOptionPane.showInternalMessageDialog(null, "Error: debe ingresar solo números.");
+        }
         return opcion;
     }
-
     public String nombreRegis(){
-        x.nextLine();
-        System.out.println("Nombre: ");
-        String nombreApellido = x.nextLine();
+        String nombreApellido = JOptionPane.showInputDialog("Nombre: ");
         return nombreApellido;
     }
     public String nombreUsuarioRegis(){
-        System.out.println("Nombre de usuario: ");
-        String nombreUsuario = x.next();
+        String nombreUsuario = JOptionPane.showInputDialog("Nombre de usuario: ");
         return nombreUsuario;
     }
     public String correoRegis(){
-        System.out.println("Correo: ");
-        String correo = x.next();
+        String correo = JOptionPane.showInputDialog("Correo: ");
         return correo;
     }
     public String contraseniaRegis(){
-        System.out.println("La contraseña debe tener al menos una mayúscula, una minúscula, un número y un " +
+        JOptionPane.showInternalMessageDialog(null,"La contraseña debe tener al menos una mayúscula, una minúscula, un número y un " +
                 "caracter especial. Debe tener MÍNIMO 8 carateres. ");
-        System.out.println("Contraseña: ");
-        String clave = x.next();
+        String clave = JOptionPane.showInputDialog("Contraseña: ");
         return clave;
     }
     public String datoInicioSesion(){
-        System.out.println("\nIngrese su nombre de usuario o correo");
-        String dato = x.next();
+        String dato = JOptionPane.showInputDialog("\nIngrese su nombre de usuario o correo");
         return  dato;
     }
     public String claveInicioSesion(){
-        System.out.println("Ingrese su clave");
-        String clave = x.next();
+        String clave = JOptionPane.showInputDialog("Ingrese su clave");
         return clave;
     }
-    public int interfazUsuario(String nameUsuario) {
-        int opcion;
-        do {
-            System.out.println("\nMenú usuario");
-            System.out.println("1. Ver perfil");
-            System.out.println("2. Actualizar datos");
-            System.out.println("3. Buscar amigos");
-            System.out.println("4. Realizar test");
-            System.out.println("5. Eliminar cuenta");
-            System.out.println("6. Desactivar cuenta");
-            System.out.println("7. Ver novedades");
-            System.out.println("8. Buscar novedades");
-            System.out.println("0. Salir");
-            System.out.print("Elija una opción: ");
-            opcion = x.nextInt();
-        } while (opcion < 0 || opcion > 8);
+    public int interfazUsuario() {
+        int opcion = 0;
+        try {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("\nMenú usuario: \n1. Ver perfil. \n2. Actualizar datos." +
+                    "\n3. Buscar amigos. \n4. Realizar test. \n5. Eliminar cuenta. \n6. Desactivar cuenta. \n7. Ver novedades." +
+                    "\n8. Buscar novedades \n0. Salir. \nElija una opción: "));
+        }catch (NumberFormatException e) {
+            JOptionPane.showInternalMessageDialog(null, "Error: debe ingresar solo números.");
+        }
         return opcion;
     }
     public String datoActualizar() {
-        System.out.println("¿Qué dato desea actualizar?(nombre, usuario, " +
+        String actualizarDato = JOptionPane.showInputDialog("¿Qué dato desea actualizar?(nombre, usuario, " +
                 "correo, contraseña)");
-        String actualizarDato = x.next();
         return actualizarDato;
     }
     public String nuevoNombre(){
-        x.nextLine();
-        System.out.println("Ingrese el nombre:");
-        String nombre = x.nextLine();
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre:");
         return nombre;
     }
     public String nuevoUsuario(){
-        System.out.println("Ingrese el nombre de usuario:");
-        String nombreUsuario = x.next();
+        String nombreUsuario = JOptionPane.showInputDialog("Ingrese el nombre de usuario:");
         return nombreUsuario;
     }
     public String nuevoCorreo(){
-        System.out.println("Ingrese el correo:");
-        String correo = x.next();
+        String correo = JOptionPane.showInputDialog("Ingrese el correo:");
         return correo;
     }
     public String nuevaContrasenia(){
-        System.out.println("Ingrese la contraseña:");
-        String contrasenia = x.next();
+        String contrasenia = JOptionPane.showInputDialog("Ingrese la contraseña:");
         return contrasenia;
     }
     public String buscarAmigo(){
-        System.out.println("\nIngrese el nombre de usuario: ");
-        String nameUsuario = x.next();
+        String nameUsuario = JOptionPane.showInputDialog("\nIngrese el nombre de usuario: ");
         return nameUsuario;
     }
     public String eliminarCuenta(){
-        System.out.println("\n¿Desea eliminar su cuenta?");
-        String respuesta = x.next();
+        String respuesta = JOptionPane.showInputDialog("\n¿Desea eliminar su cuenta?");
         return respuesta;
     }
     public String desactivarCuenta(){
-        System.out.println("\n¿Desea desactivar su cuenta?");
-        String respuesta = x.next();
+        String respuesta = JOptionPane.showInputDialog("\n¿Desea desactivar su cuenta?");
         return respuesta;
     }
     public String buscarNovedad(){
-        System.out.println("Ingrese el titulo o subtitulo de la novedad");
-        String tema = x.nextLine().toUpperCase();
+        String tema = JOptionPane.showInputDialog("Ingrese el titulo o subtitulo de la novedad");
         return tema;
     }
     public void imprimir(String texto){
-        System.out.println(texto);
+        JOptionPane.showInternalMessageDialog(null, texto);
     }
     public void conectar(){
         Controlador control = new Controlador();
