82e8042ac903fea7294a8488a7eacd11a64cf0a4
Leidy Johanna Bolivar Gonzalez <140461573+JohannaB95@users.noreply.github.com>
2024-06-20 18:45:01 -0500

    Update InterfazAdministrador.java

--- src/Vista/InterfazAdministrador.java
+++ src/Vista/InterfazAdministrador.java
@@ -1,5 +1,6 @@
 package Vista;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class InterfazAdministrador {
@@ -8,84 +9,79 @@ public class InterfazAdministrador {
 
     public int menu(){
         int opc = 0;
-        System.out.println("\nMenú Administrador");
-        System.out.println("1. Test\n2. Busqueda de usuarios");
-        opc = x.nextInt();
-
+        try {
+            opc = Integer.parseInt(JOptionPane.showInputDialog("\nMenú Administrador \n1. Test \n2. Busqueda de usuarios"));
+        }catch (NumberFormatException e) {
+            JOptionPane.showInternalMessageDialog(null, "Error: debe ingresar solo números.");
+        }
         return opc;
     }
     public  String id(){
         String id = "";
-        System.out.println("Ingrese el numero de la pregunta");
-        id = x.next();
+        id = JOptionPane.showInputDialog("Ingrese el numero de la pregunta");
         return id;
     }
     public  String preguntas(){
         String pregunta = "";
-        System.out.println("Ingrese la pregunta");
-        x.nextLine();
-        pregunta = x.nextLine();
+        pregunta = JOptionPane.showInputDialog("Ingrese la pregunta");
         return  pregunta;
     }
     public String respuestas1(){
         String pregunta = "";
-        System.out.println("Ingrese la respuesta 1");
-        pregunta = x.nextLine();
+        pregunta = JOptionPane.showInputDialog("Ingrese la respuesta 1");
         return  pregunta;
     }
     public String respuestas2(){
         String pregunta = "";
-        System.out.println("Ingrese la respuesta 2");
-        pregunta = x.nextLine();
+        pregunta = JOptionPane.showInputDialog("Ingrese la respuesta 2");
         return  pregunta;
     }
     public String respuestas3(){
         String pregunta = "";
-        System.out.println("Ingrese la respuesta 3");
-        pregunta = x.nextLine();
+        pregunta = JOptionPane.showInputDialog("Ingrese la respuesta 3");
         return  pregunta;
 
     }
     public String respuestas(){
         String pregunta = "";
-        System.out.println("Ingrese la respuesta correcta");
-        pregunta = x.nextLine();
+        pregunta = JOptionPane.showInputDialog("Ingrese la respuesta correcta");
         return  pregunta;
     }
     public int test(){
         int opc = 0;
-
-        System.out.println("1. Editar el test\n2. Añadir nueva pregunta" +
-                "\n3. Eliminar pregunta\n4. Buscar preguntas\n0. Volver al menu");
-        opc = x.nextInt();
+        try {
+            opc = Integer.parseInt(JOptionPane.showInputDialog("1. Editar el test\n2. Añadir nueva pregunta" +
+                "\n3. Eliminar pregunta\n4. Buscar preguntas\n0. Volver al menu"));
+        }catch (NumberFormatException e) {
+            JOptionPane.showInternalMessageDialog(null, "Error: debe ingresar solo números.");
+        }
         return  opc;
     }
     public String seleccion(){
-        System.out.println("Ingrese el numero de pregunta a cambiar");
-        return  x.next();
+        String opcion = JOptionPane.showInputDialog("Ingrese el numero de pregunta a cambiar");
+        return  opcion;
     }
     public  int seleccio(){
         int opc = 0;
-        System.out.println("1. Pregunta\n2. Respuesta 1 \n3. Respuesta 2" +
-                "\n4.Respuesta 3\n5. Respuesta correcta");
-        opc = x.nextInt();
+        try {
+            opc = Integer.parseInt(JOptionPane.showInputDialog("1. Pregunta\n2. Respuesta 1 \n3. Respuesta 2" +
+                "\n4.Respuesta 3\n5. Respuesta correcta"));
+        }catch (NumberFormatException e) {
+            JOptionPane.showInternalMessageDialog(null, "Error: debe ingresar solo números.");
+        }
         return  opc;
     }
     public String respuesta(){
         String respues = "";
-        System.out.println("Ingrese el nuevo valor");
-        x.nextLine();
-        respues = x.nextLine();
+        respues = JOptionPane.showInputDialog("Ingrese el nuevo valor");
         return respues;
     }
     public String usu(){
         String usuario = "";
-        System.out.println("Ingrese el nombre de usuario");
-        usuario = x.next();
+        usuario = JOptionPane.showInputDialog("Ingrese el nombre de usuario");
         return usuario;
     }
     public void imprimir(String valor){
-        System.out.println(valor);
+        JOptionPane.showInternalMessageDialog(null, valor);
     }
-
 }
