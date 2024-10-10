4cd3d76f363c0349a5c2d9f2740506b472d531e0
Leidy Johanna Bolivar Gonzalez <140461573+JohannaB95@users.noreply.github.com>
2024-10-10 13:19:45 -0500

    Update InterfazAdministrador.java

--- src/Vista/InterfazAdministrador.java
+++ src/Vista/InterfazAdministrador.java
@@ -42,7 +42,9 @@ public class InterfazAdministrador {
     public int test() {
         int opc = 0;
         try {
-            opc = Integer.parseInt(JOptionPane.showInputDialog(null, "1. Editar el test\n2. Añadir nueva pregunta\n3. Eliminar pregunta\n4. Buscar preguntas\n0. Volver al menu"));
+            opc = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "1. Editar el test\n2. Añadir nueva pregunta\n3. Eliminar pregunta\n" +
+                            "4. Buscar preguntas\n0. Volver al menu"));
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "Error: debe ingresar solo números.");
         }
@@ -72,34 +74,46 @@ public class InterfazAdministrador {
         return JOptionPane.showInputDialog("Ingrese el nombre de usuario");
     }
 
-    public String clave() {
-        return JOptionPane.showInputDialog("Ingrese el numero de la novedad");
-    }
-
     public String titulo() {
-        return JOptionPane.showInputDialog("Ingrese el titulo del contenido").toUpperCase();
+        String titulo = JOptionPane.showInputDialog("Ingrese el titulo del contenido: ");
+        System.out.println("Título ingresado: " + titulo);
+        return titulo;
     }
-
-    public String subtitulo() {
-        return JOptionPane.showInputDialog("Ingrese el subtitulo del contenido").toUpperCase();
+    public String descripcion() {
+        return JOptionPane.showInputDialog("Ingrese el contenido de la novedad: ");
     }
-
-    public String conten() {
-        return JOptionPane.showInputDialog("Ingrese el contenido de la novedad");
+    public String autor() {
+        return JOptionPane.showInputDialog("Ingrese el nombre del autor: ");
+    }
+    public String url() {
+        return JOptionPane.showInputDialog("Ingrese la url: ");
+    }
+    public String fechaPublic() {
+        return JOptionPane.showInputDialog("Ingrese la fecha: ");
+    }
+    public String tipoContent() {
+        return JOptionPane.showInputDialog("Ingrese el tipo de contenido: ");
+    }
+    public String contDetalle() {
+        return JOptionPane.showInputDialog("Ingrese los detalles del contenido: ");
+    }
+    public String categoria() {
+        return JOptionPane.showInputDialog("Ingrese la categoria del contenido: ");
     }
 
     public String busc() {
-        return JOptionPane.showInputDialog("Titulo o subtitulo a buscar").toUpperCase();
+        return JOptionPane.showInputDialog("Titulo o subtitulo a buscar");
     }
 
     public String elim() {
-        return JOptionPane.showInputDialog("Titulo o subtitulo a eliminar").toUpperCase();
+        return JOptionPane.showInputDialog("Titulo o subtitulo a eliminar");
     }
 
     public int contenido() {
         int opc = 0;
         try {
-            opc = Integer.parseInt(JOptionPane.showInputDialog(null, "1. Editar el contenido\n2. Añadir nuevo contenido\n3. Eliminar contenido\n4. Buscar novedad\n0. Volver al menu"));
+            opc = Integer.parseInt(JOptionPane.showInputDialog(null, "1. Editar el contenido\n" +
+                    "2. Añadir nuevo contenido\n3. Eliminar contenido\n4. Buscar novedad\n0. Volver al menu"));
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "Error: debe ingresar solo números.");
         }
@@ -107,15 +121,25 @@ public class InterfazAdministrador {
     }
 
     public String actualizar() {
-        return JOptionPane.showInputDialog("Ingrese el titulo o subtitulo a cambiar").toUpperCase();
+        return JOptionPane.showInputDialog("Ingrese el titulo ");
     }
 
     public int cambio() {
         int opc = 0;
-        try {
-            opc = Integer.parseInt(JOptionPane.showInputDialog(null, "1. Titulo\n2. Subtitulo \n3. Contenido"));
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Error: debe ingresar solo números.");
+        boolean valido = false;
+        while (!valido) {
+            try {
+                opc = Integer.parseInt(JOptionPane.showInputDialog(null, "1. Titulo\n" +
+                        "2. Descripción \n3. Autor \n4. Url \n5. Fecha Publicación \n6. Tipo de " +
+                        "contenido \n7. Detalle del contenido \n8. Categoria"));
+
+                if (opc < 1 || opc > 8) {
+                    throw new NumberFormatException();
+                }
+                valido = true;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Error: debe ingresar un número entre 1 y 8.");
+            }
         }
         return opc;
     }
@@ -128,3 +152,4 @@ public class InterfazAdministrador {
         JOptionPane.showMessageDialog(null, valor);
     }
 }
+
